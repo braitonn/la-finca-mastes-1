74664c5a83f3a3fe58c650645d450973c575ddb2
braitonn <braitonmartinez@institutocedec.com>
2023-10-17 02:06:32 -0300

    sombras estilos y colores 18/10

---
+++ api/config.php
@@ -0,0 +1,8 @@
+<?php
+/* Definimos las constantes de conexión */
+define('DB_HOST', 'localhost'); // Servidor MySQL
+define('DB_USER', 'root'); // Usuario del servidor MySQL
+define('DB_PASS', ''); // Contraseña del servidor MySQL
+define('DB_NAME', 'lafinca'); // Nombre de la Base de Datos
+define('DB_CHARSET', 'utf8'); // Cotejamiento del conjunto de caracteres
+?>
